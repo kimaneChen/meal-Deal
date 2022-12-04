ded373e535c1e7ac42b69b0775d6c7353d9f78a3
Kimane <chenjx1024@outlook.com>
2022-12-04 15:03:16 +1000

    feat: build the new version

--- components/CategoryGridTile.tsx
+++ components/CategoryGridTile.tsx
@@ -1,4 +1,4 @@
-import { Pressable, View, Text, StyleSheet } from 'react-native';
+import { Pressable, View, Text, StyleSheet, Platform } from 'react-native';
 
 export interface CategoryGridTileProps {
   title?: string;
@@ -8,9 +8,14 @@ export interface CategoryGridTileProps {
 function CategoryGridTile(props: CategoryGridTileProps) {
   const { title, color } = props;
   return (
-    <View style={[styles.itemContainer, { backgroundColor: color }]}>
-      <Pressable>
-        <View style={styles.innerContainer}>
+    <View style={styles.itemContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed ? styles.buttonPressed : styles.button
+        }
+        android_ripple={{ color: '#ccc' }}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
@@ -22,15 +27,33 @@ export default CategoryGridTile;
 
 const styles = StyleSheet.create({
   itemContainer: {
-    margin: 24,
+    flex: 1,
+    margin: 16,
+    height: 150,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
   },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    flex: 1,
+    opacity: 0.75,
+  },
+
   innerContainer: {
-    height: 100,
-    width: 80,
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
-    color: 'white',
     fontWeight: 'bold',
-    fontSize: 16,
+    fontSize: 18,
   },
 });
