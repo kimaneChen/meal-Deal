4ab59d473defd9b1e4ca4896e159bcf9a6e05b56
Kimane <chenjx1024@outlook.com>
2022-12-08 12:51:26 +1000

    fix/solve the styles with nested nav

--- App.tsx
+++ App.tsx
@@ -5,20 +5,54 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import { Ionicons } from '@expo/vector-icons';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverview from './screens/MealsOverview';
-import type { RootStackParamList } from './types';
+import type { RootStackParamList, DrawerParamList } from './types';
 import MealDetails from './screens/MealDetails';
+import FavoritesScreen from './screens/FavoriateMeal';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
-const Drawer = createDrawerNavigator();
-// function DrawerNavigator() {
-//   return (
-//     <Drawer.Navigator>
-//       <Drawer.Screen name="DrawerCategories" component={CategoriesScreen} />
-//     </Drawer.Navigator>
-//   );
-// }
+const Drawer = createDrawerNavigator<DrawerParamList>();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#351401',
+        },
+        headerTintColor: 'white',
+        sceneContainerStyle: { backgroundColor: '#3f2f25' },
+        drawerContentStyle: { backgroundColor: '#3f2f25' },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#351401',
+        drawerActiveBackgroundColor: '#e4baa1',
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          title: 'Favorite Meals',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -36,9 +70,9 @@ export default function App() {
         >
           <Stack.Screen
             name="MealCategories"
-            component={CategoriesScreen}
+            component={DrawerNavigator}
             options={{
-              title: 'Meal Categories',
+              headerShown: false,
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverview} />
--- types.ts
+++ types.ts
@@ -1,15 +1,20 @@
 import 'react-native-gesture-handler';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import type { DrawerScreenProps } from '@react-navigation/drawer';
+import {
+  NavigatorScreenParams,
+  CompositeScreenProps,
+} from '@react-navigation/native';
 
 export type RootStackParamList = {
-  MealCategories: undefined;
+  MealCategories: NavigatorScreenParams<DrawerParamList>;
   MealsOverview: { categoryId: string };
   MealDetails: { mealID: string };
 };
 
-export type CategoriesScreenProps = NativeStackScreenProps<
-  RootStackParamList,
-  'MealCategories'
+export type CategoriesScreenProps = CompositeScreenProps<
+  DrawerScreenProps<DrawerParamList, 'Categories'>,
+  NativeStackScreenProps<RootStackParamList>
 >;
 
 export type MealsOverviewProps = NativeStackScreenProps<
@@ -21,3 +26,10 @@ export type MealDetailsProps = NativeStackScreenProps<
   RootStackParamList,
   'MealDetails'
 >;
+
+export type DrawerParamList = {
+  Categories: undefined;
+  Favorites: undefined;
+};
+
+export type FavoritesProps = DrawerScreenProps<DrawerParamList, 'Favorites'>;
